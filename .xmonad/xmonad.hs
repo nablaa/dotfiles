6e1d67c51ac80ec2be4a69662c1fb56d19fe66ea
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2014-03-23 18:10:30 +0200

    xmonad: Add more workspaces

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -30,7 +30,7 @@ myModMask = mod1Mask  -- rebind Mod to Super key
 myTerminal = "xterm"
 myTerminalBig = "xterm -fs 20"
 myBorderWidth = 2
-myWorkspaces = ["1:web", "2:irc", "3:terms", "4:term", "5:code", "6:dev", "7:misc", "8:music", "9:vbox"]
+myWorkspaces = ["1:web", "2:irc", "3:terms", "4:term", "5:code", "6:dev", "7:misc", "8:music", "9:vbox", "0:empty", "+:empty"]
 
 myLayoutHook = onWorkspace "1:web" webLayout $ onWorkspace "2:irc" noTitleLayout $ onWorkspace "3:terms" noTitleLayout $ onWorkspace "4:term" fullLayout $ onWorkspace "9:vbox" fullLayout $ layouts
     where layouts = smartBorders $ avoidStruts $ (layoutHook defaultConfig ||| Grid ||| ThreeCol 1 (3/100) (1/2))
@@ -107,6 +107,10 @@ myKeys =
     , ((0, 0x1008FF13), spawn "amixer set Master 2+ && notify-send Volume 'Raise volume'") -- XF86XK_AudioRaiseVolume
     , ((0, 0x1008FF12), spawn "amixer set Master toggle && amixer set PCM unmute && notify-send Volume 'Toggle mute'") -- XF86XK_AudioMute
     ]
+    ++
+    [((m .|. myModMask, k), windows $ f i)
+    | (i, k) <- zip myWorkspaces [xK_1, xK_2, xK_3, xK_4, xK_5, xK_6, xK_7, xK_8, xK_9, xK_0, xK_plus]
+    , (f, m) <- [(greedyView, 0), (shift, shiftMask)]]
 
 myXPConfig = defaultXPConfig {
       font = "xft: inconsolata-14"
