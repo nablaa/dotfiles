6d52302c3860a1f0c56e2999085a56d4ce120827
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2015-03-13 19:52:00 +0200

    xmonad: Cleanup config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -6,33 +6,24 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Grid
-import XMonad.Layout.Tabbed
 import XMonad.Layout.ThreeColumns
 import XMonad.Layout.PerWorkspace (onWorkspace)
 import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.SetWMName
 import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.ICCCMFocus
 import XMonad.StackSet hiding (workspaces)
 import XMonad.Actions.SpawnOn
-import XMonad.Actions.CopyWindow
-import XMonad.Actions.GridSelect
-import XMonad.Prompt
-import XMonad.Prompt.Man
-import XMonad.Prompt.RunOrRaise
-import Data.List
 import Data.Monoid
-import Control.Concurrent
-import Data.Ratio ((%))
 
 myModMask = mod1Mask  -- rebind Mod to Super key
 myTerminal = "xterm"
-myTerminalBig = "xterm -fs 20"
 myBorderWidth = 2
-myWorkspaces = ["1:web", "2:irc", "3:terms", "4:term", "5:code", "6:dev", "7:misc", "8:music", "9:vbox", "0:empty", "+:empty"]
+myWorkspaces = ["1:web", "2:irc", "3:terms", "4:term", "5:code", "6:dev",
+                "7:misc", "8:music", "9:vbox", "0:empty", "+:empty"]
 
-myLayoutHook = onWorkspace "1:web" webLayout $ onWorkspace "2:irc" noTitleLayout $ onWorkspace "3:terms" noTitleLayout $ onWorkspace "4:term" fullLayout $ onWorkspace "9:vbox" fullLayout $ layouts
+myLayoutHook = onWorkspace "1:web" webLayout $ onWorkspace "2:irc" noTitleLayout $
+               onWorkspace "3:terms" noTitleLayout $ onWorkspace "4:term" fullLayout $
+               onWorkspace "9:vbox" fullLayout $ layouts
     where layouts = smartBorders $ avoidStruts $ (layoutHook defaultConfig ||| Grid ||| ThreeCol 1 (3/100) (1/2))
           webLayout = smartBorders $ avoidStruts (Tall 1 (3/100) (70/100) ||| Full)
           fullLayout = smartBorders $ noBorders Full
@@ -45,9 +36,6 @@ myManageHook = (composeAll . concat $
     , className =? "Zenity"     --> doCenterFloat
     , className =? "feh"        --> doCenterFloat
     , className =? "VirtualBox" --> doShift "9:vbox"
-    , className =? "banshee"    --> doShift "8:music"
-    , className =? "Banshee"    --> doShift "8:music"
-    , className =? "banshee-1"  --> doShift "8:music"
     , className =? "Xfce4-notifyd" --> doIgnore -- Fixes notification bubbles grabbing focus
     , title     =? "Save As..." --> doCenterFloat
     , title     =? "Save File"  --> doCenterFloat
@@ -56,15 +44,7 @@ myManageHook = (composeAll . concat $
     , title     =? "xterm_4"  --> doShift "4:term"
     , title     =? "xterm_5"  --> doShift "5:code"
     , title     =? "xterm_8"  --> doShift "8:music"
-    ]]) <+> myFloats <+> manageDocks <+> manageHook defaultConfig
-
-myFloats = composeAll . concat $
-    [ [ fmap (c `isInfixOf`) className       --> doFloat | c <- myFloats ]
-    , [ fmap (t `isInfixOf`) title           --> doFloat | t <- myOtherFloats ]
-    ]
-    where
-        myFloats        = ["TopLevelShell", "Blender:Render", "gimp", "Gimp"]
-        myOtherFloats   = ["Scope", "Editor", "Simulink Library Browser", "Figure ", "Blender:Render", "Chess"]
+    ]]) <+> manageDocks <+> manageHook defaultConfig
 
 myLogHook xmproc = dynamicLogWithPP $ xmobarPP {
                      ppOutput = hPutStrLn xmproc
@@ -87,36 +67,27 @@ myFocusedBorderColor = "#FF0000"
 
 myKeys =
     [ ((myModMask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock")
-    , ((controlMask, xK_Print), spawn "scrot -s")
-    , ((0, xK_Print), spawn "scrot")
     , ((myModMask .|. shiftMask, xK_s), sendMessage ToggleStruts)
     , ((myModMask .|. shiftMask, xK_p), startupPrograms)
     , ((myModMask .|. shiftMask, xK_f), fullFloatFocused)
     , ((myModMask, xK_p), spawn "dmenu_run -i -fn \'-*-fixed-*-*-*-20-*-*-*-*-*-iso8859-15\'")
-    , ((myModMask .|. shiftMask, xK_t), spawn myTerminalBig)
     , ((myModMask .|. shiftMask, xK_b), spawn "ncmpcpp-status")
     , ((myModMask .|. shiftMask, xK_n), spawn "ncmpcpp pause")
     , ((myModMask .|. shiftMask, xK_m), spawn "ncmpcpp play")
     , ((myModMask .|. shiftMask, xK_comma), spawn "ncmpcpp prev")
     , ((myModMask .|. shiftMask, xK_period), spawn "ncmpcpp next")
-    , ((myModMask .|. controlMask, xK_x), runOrRaisePrompt myXPConfig)
-    , ((myModMask, xK_F1), manPrompt myXPConfig)
-    , ((myModMask, xK_u), goToSelected defaultGSConfig)
-    , ((myModMask, xK_s), spawnSelected defaultGSConfig ["xterm", "gvim", "firefox"])
     , ((0, 0x1008FF11), spawn "amixer set Master 2- && notify-send Volume 'Lower volume'") -- XF86XK_AudioLowerVolume
     , ((0, 0x1008FF13), spawn "amixer set Master 2+ && notify-send Volume 'Raise volume'") -- XF86XK_AudioRaiseVolume
     , ((0, 0x1008FF12), spawn "amixer set Master toggle && amixer set PCM unmute && notify-send Volume 'Toggle mute'") -- XF86XK_AudioMute
     ]
+    -- More workspaces
     ++
     [((m .|. myModMask, k), windows $ f i)
     | (i, k) <- zip myWorkspaces [xK_1, xK_2, xK_3, xK_4, xK_5, xK_6, xK_7, xK_8, xK_9, xK_0, xK_plus]
     , (f, m) <- [(greedyView, 0), (shift, shiftMask)]]
 
-myXPConfig = defaultXPConfig {
-      font = "xft: inconsolata-14"
-    , position = Top
-    , promptBorderWidth = 0
-}
+fullFloatFocused =
+    withFocused $ \f -> windows =<< appEndo `fmap` runQuery doFullFloat f
 
 startupPrograms = do
                   spawn (myTerminal ++ " -title xterm_2")
@@ -126,7 +97,6 @@ startupPrograms = do
                   spawn (myTerminal ++ " -title xterm_5")
                   spawn (myTerminal ++ " -title xterm_8")
                   spawn (myTerminal ++ " -title xterm_8")
-                  spawn "gnome-gmail-notifier"
                   spawn "pidgin"
                   spawnOn "1:web" "firefox"
 
@@ -139,10 +109,6 @@ main = do
              , borderWidth = myBorderWidth
              , focusedBorderColor = myFocusedBorderColor
              , modMask = myModMask
-             , logHook = myLogHook xmproc >> ewmhDesktopsLogHook >> setWMName "LG3D" >> takeTopFocus
-             --, startupHook = setWMName "LG3D" -- Fix Java programs
+             , logHook = myLogHook xmproc >> ewmhDesktopsLogHook >> setWMName "LG3D"
              , workspaces = myWorkspaces
              } `additionalKeys` myKeys
-
-fullFloatFocused =
-    withFocused $ \f -> windows =<< appEndo `fmap` runQuery doFullFloat f
